f1aa2b44070393ae4220cb6e1b783888484d16df
Julian Daberkow <jdaberkow@techfak.uni-bielefeld.de>
2015-11-08 02:31:20 +0100

    Add method to test communication with LocationRegistry

--- src/main/java/org/dc/bco/bcozy/BCozy.java
+++ src/main/java/org/dc/bco/bcozy/BCozy.java
@@ -18,13 +18,23 @@
  */
 package org.dc.bco.bcozy;
 
+import de.citec.jps.core.JPService;
+import de.citec.jps.preset.JPDebugMode;
+import de.citec.jul.exception.CouldNotPerformException;
+import de.citec.jul.exception.printer.ExceptionPrinter;
+import de.citec.jul.exception.printer.LogLevel;
+import de.citec.jul.extension.rst.processing.MetaConfigVariableProvider;
+import de.citec.lm.remote.LocationRegistryRemote;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rst.spatial.LocationConfigType;
+
+import java.util.List;
 
 /**
  * Main Class of the BCozy Program.
@@ -37,27 +47,31 @@ public class BCozy extends Application {
     public static final String APP_NAME = BCozy.class.getSimpleName().toLowerCase();
 
     /**
+     * Application logger.
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(BCozy.class);
+
+    /**
      * Main Method starting JavaFX Environment.
      *
      * @param args Arguments from commandline.
      */
     public static void main(final String... args) {
 
-// // TODO julian: please enable code after your commit!
-//        logger.info("Start " + APP_NAME + "...");
-//
-//        /* Setup JPService */
-//        JPService.setApplicationName(APP_NAME);
-//        JPService.registerProperty(JPDebugMode.class);
-//        JPService.parseAndExitOnError(args);
-//
-//        try {
-        launch(args);
-//        } catch (InitializationException ex) {
-//            throw ExceptionPrinter
-//                    .printHistoryAndReturnThrowable(ex, logger, LogLevel.ERROR);
-//        }
-//        logger.info(APP_NAME + " successfully started.");
+        LOGGER.info("Start " + APP_NAME + "...");
+
+        /* Setup JPService */
+        JPService.setApplicationName(APP_NAME);
+        JPService.registerProperty(JPDebugMode.class);
+        JPService.parseAndExitOnError(args);
+
+        //try {
+            launch(args);
+        //} catch (InitializationException ex) {
+        //    throw ExceptionPrinter
+        //            .printHistoryAndReturnThrowable(ex, LOGGER, LogLevel.ERROR);
+        //}
+        LOGGER.info(APP_NAME + " finished.");
 
     }
 
@@ -69,17 +83,41 @@ public class BCozy extends Application {
         primaryStage.setTitle("BCozy");
         final Button btn = new Button();
         btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(final ActionEvent event) {
-                //System.out.println("Hello World!");
-            }
-        });
+        btn.setOnAction(event -> LOGGER.info("Hello World!"));
 
         final StackPane root = new StackPane();
         root.getChildren().add(btn);
         primaryStage.setScene(new Scene(root, screenWidth, screenHeight));
         primaryStage.show();
+
+        try {
+            testLocationRegistry();
+        } catch (InterruptedException e) {
+            ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+        }
+    }
+
+    private void testLocationRegistry() throws InterruptedException {
+        try {
+            LocationRegistryRemote locationRegistryRemote = new LocationRegistryRemote();
+
+            locationRegistryRemote.init();
+            locationRegistryRemote.activate();
+            List<LocationConfigType.LocationConfig> list = locationRegistryRemote.getLocationConfigs();
+            for (LocationConfigType.LocationConfig locationConfig : list) {
+                System.out.println(locationConfig.getLabel());
+                MetaConfigVariableProvider metaConfigVariableProvider =
+                        new MetaConfigVariableProvider("locationConfig", locationConfig.getMetaConfig());
+                try {
+                    LOGGER.info("Found test entry: " + metaConfigVariableProvider.getValue("TEST_ENTRY"));
+                } catch (CouldNotPerformException e) {
+
+                }
+            }
+            locationRegistryRemote.shutdown();
+
+        } catch (CouldNotPerformException e) {
+            ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+        }
     }
 }
