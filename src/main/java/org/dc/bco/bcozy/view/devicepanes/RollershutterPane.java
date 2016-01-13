3f7a50c144b2685e0e87009490da561f3e5fe337
agatting <agatting@techfak.uni-bielefeld.de>
2016-01-13 19:55:29 +0100

    added rollerShutterPane content

--- src/main/java/org/dc/bco/bcozy/view/devicepanes/RollershutterPane.java
+++ src/main/java/org/dc/bco/bcozy/view/devicepanes/RollershutterPane.java
@@ -18,21 +18,28 @@
  */
 package org.dc.bco.bcozy.view.devicepanes;
 
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import org.dc.bco.bcozy.view.SVGIcon;
 import org.dc.bco.dal.remote.unit.DALRemoteService;
 import org.dc.bco.dal.remote.unit.RollershutterRemote;
 import org.dc.jul.exception.CouldNotPerformException;
 import org.dc.jul.exception.printer.ExceptionPrinter;
 import org.dc.jul.exception.printer.LogLevel;
 import org.dc.jul.pattern.Observable;
-import javafx.geometry.Orientation;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+//import javafx.scene.image.Image;
 import javafx.scene.layout.*;
-import org.controlsfx.control.PlusMinusSlider;
+//import javafx.scene.paint.Color;
 import org.dc.bco.bcozy.view.Constants;
+//import org.dc.bco.bcozy.view.ImageEffect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rst.homeautomation.state.ShutterStateType;
+//import rst.homeautomation.unit.RollershutterType;
 
 /**
  * Created by hoestreich on 11/19/15.
@@ -42,9 +49,10 @@ public class RollershutterPane extends UnitPane {
     private static final Logger LOGGER = LoggerFactory.getLogger(RollershutterPane.class);
 
     private final RollershutterRemote rollershutterRemote;
-    private final Image imageIcon;
-    private BorderPane headContent;
-    private BorderPane bodyContent;
+    private final BorderPane headContent;
+    private final HBox bodyContent;
+    //private final Image bottomImage;
+    //private final Image topImage;
 
     /**
      * Constructor for a RollershutterPane.
@@ -52,7 +60,12 @@ public class RollershutterPane extends UnitPane {
      */
     public RollershutterPane(final DALRemoteService rollershutterRemote) {
         this.rollershutterRemote = (RollershutterRemote) rollershutterRemote;
-        imageIcon = new Image("/icons/shutter.png");
+
+        //bottomImage = new Image("/icons/shutter_test.png");
+        //topImage    = new Image("/icons/shutter_test2.png");
+
+        headContent = new BorderPane();
+        bodyContent = new HBox();
 
         try {
             super.setUnitLabel(this.rollershutterRemote.getData().getLabel());
@@ -63,62 +76,104 @@ public class RollershutterPane extends UnitPane {
 
         initTitle();
         initContent();
-        final Pane pane = new Pane();
-        pane.getChildren().add(bodyContent);
-        createWidgetPane(headContent, pane);
+        createWidgetPane(headContent, bodyContent);
+
+        this.rollershutterRemote.addObserver(this);
     }
-
-
 
     @Override
     protected void initTitle() {
-        final ImageView rollershutterIcon;
-        final BorderPane borderPane;
+        //final ImageEffect imageGroup = new ImageEffect(bottomImage, topImage, Color.BLACK);
 
-        //image
-        rollershutterIcon = new ImageView(imageIcon);
-        //rollershutterIcon.setClip(new ImageView(icon));
-        rollershutterIcon.setFitHeight(Constants.MIDDLE_ICON);
-        rollershutterIcon.setFitWidth(Constants.MIDDLE_ICON);
-
-        //borderPane for header of titledPane as "graphic"
-        borderPane = new BorderPane();
-        borderPane.setLeft(rollershutterIcon);
-        borderPane.setCenter(new Label(super.getUnitLabel()));
-
-        //this.getStyleClass().add("widgetPane");
-        //this.setGraphic(borderPane);
-        //this.headPart(borderPane);
-        headContent = borderPane;
+        //headContent.setLeft(imageGroup);
+        headContent.setCenter(new Label(super.getUnitLabel()));
+        //headContent.prefHeightProperty().set(bottomImage.getHeight() + (Constants.INSETS * 2));
+        //CHECKSTYLE.OFF: MagicNumber
+        headContent.prefHeightProperty().set(50 + Constants.INSETS);
+        //CHECKSTYLE.ON: MagicNumber
     }
 
     @Override
     protected void initContent() {
-        final BorderPane borderPane = new BorderPane();
-        final BorderPane verticalLayout = new BorderPane();
-        //TODO: Implement Icon class which adapts its size depending on the window size (responsive)
-        final Label actualValue = new Label("Aktueller Wert: 15");
-                                            // + Double.toString(shutterInstance.getOpeningRatio()));
-        final ImageView imageViewIcon = new ImageView(imageIcon);
-        imageViewIcon.setFitHeight(Constants.MIDDLE_ICON);
-        imageViewIcon.setFitWidth(Constants.MIDDLE_ICON);
+        final Button buttonUp = new Button();
+        final Button buttonDown = new Button();
+        final Button buttonOpen = new Button();
+        final Button buttonClose = new Button();
+        final SVGIcon arrowUp;
+        final SVGIcon arrowDown;
+        final SVGIcon arrowDoubleUP;
+        final SVGIcon arrowDoubleDown;
 
-        final PlusMinusSlider control = new PlusMinusSlider();
-        control.setOrientation(Orientation.VERTICAL);
-        verticalLayout.setTop(imageViewIcon);
-        verticalLayout.setBottom(actualValue);
+        arrowUp = new SVGIcon(MaterialDesignIcon.CHEVRON_UP, Constants.SMALL_ICON, true);
+        arrowDown = new SVGIcon(MaterialDesignIcon.CHEVRON_DOWN, Constants.SMALL_ICON, true);
+        arrowDoubleUP = new SVGIcon(MaterialDesignIcon.CHEVRON_DOUBLE_UP, Constants.SMALL_ICON, true);
+        arrowDoubleDown = new SVGIcon(MaterialDesignIcon.CHEVRON_DOUBLE_DOWN, Constants.SMALL_ICON, true);
 
-        borderPane.setLeft(verticalLayout);
-        borderPane.setRight(control);
-        //this.setContent(borderPane);
-        //this.bodyPart(borderPane);
-        bodyContent = borderPane;
+        buttonUp.getStyleClass().add("roller-shutter-button");
+        buttonUp.setGraphic(arrowUp);
+        buttonDown.getStyleClass().add("roller-shutter-button");
+        buttonDown.setGraphic(arrowDown);
+        buttonOpen.getStyleClass().add("roller-shutter-button");
+        buttonOpen.setGraphic(arrowDoubleUP);
+        buttonClose.getStyleClass().add("roller-shutter-button");
+        buttonClose.setGraphic(arrowDoubleDown);
+
+        final EventHandler<MouseEvent> sendingTotalOpening = event -> {
+            try {
+                rollershutterRemote.setOpeningRatio(1.0);
+            } catch (CouldNotPerformException e) {
+                ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+            }
+        };
+
+        final EventHandler<MouseEvent> sendingTotalClosing = event -> {
+            try {
+                rollershutterRemote.setOpeningRatio(0.0);
+            } catch (CouldNotPerformException e) {
+                ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+            }
+        };
+
+        final EventHandler<MouseEvent> sendingUpLive = event -> {
+            try {
+                rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.UP);
+            } catch (CouldNotPerformException e) {
+                ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+            }
+        };
+
+        final EventHandler<MouseEvent> sendingStop = event -> {
+            try {
+                rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.STOP);
+            } catch (CouldNotPerformException e) {
+                ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+            }
+        };
+
+        final EventHandler<MouseEvent> sendingDownLive = event -> {
+            try {
+                rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.DOWN);
+            } catch (CouldNotPerformException e) {
+                ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+            }
+        };
+
+        buttonOpen.setOnMouseClicked(sendingTotalOpening);
+        buttonClose.setOnMouseClicked(sendingTotalClosing);
+        buttonUp.setOnMousePressed(sendingUpLive);
+        buttonUp.setOnMouseReleased(sendingStop);
+        buttonDown.setOnMousePressed(sendingDownLive);
+        buttonDown.setOnMouseReleased(sendingStop);
+
+        bodyContent.getChildren().addAll(buttonUp, buttonDown, buttonOpen, buttonClose);
+        bodyContent.prefHeightProperty().set(buttonUp.getPrefHeight() + bodyContent.getPadding().getTop()
+                + bodyContent.getPadding().getBottom());
     }
 
-    @Override
-    void removeObserver() {
-        //TODO: remove the observer
-    }
+//    private void initEffect() throws CouldNotPerformException {
+//        final Double openingPercentage = rollershutterRemote.getOpeningRatio();
+//        //TODO set iconRatio
+//    }
 
     @Override
     public DALRemoteService getDALRemoteService() {
@@ -126,9 +181,16 @@ public class RollershutterPane extends UnitPane {
     }
 
     @Override
+    void removeObserver() {
+        this.rollershutterRemote.removeObserver(this);
+    }
+
+    @Override
     public void update(final Observable observable, final Object rollerShutter) throws java.lang.Exception {
-        //TODO: Do something
-        //((RollershutterType.Rollershutter) rollerShutter).getLabel();
+        Platform.runLater(() -> {
+            //final Double openingPercentage = ((RollershutterType.Rollershutter) rollerShutter).getOpeningRatio();
+            //TODO set iconRatio
+        });
 
     }
 }
