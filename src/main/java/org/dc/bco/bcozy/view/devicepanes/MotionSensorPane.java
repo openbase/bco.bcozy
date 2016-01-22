fe22066cd9e33cacddc4a09f3ab0b681cb4bad53
tmichalski <tmichalski@techfak.uni-bielefeld.de>
2016-01-22 10:59:41 +0100

    Add MotionSensorPane

---
+++ src/main/java/org/dc/bco/bcozy/view/devicepanes/MotionSensorPane.java
@@ -0,0 +1,144 @@
+/**
+ * ==================================================================
+ *
+ * This file is part of org.dc.bco.bcozy.
+ *
+ * org.dc.bco.bcozy is free software: you can redistribute it and modify
+ * it under the terms of the GNU General Public License (Version 3)
+ * as published by the Free Software Foundation.
+ *
+ * org.dc.bco.bcozy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with org.dc.bco.bcozy. If not, see <http://www.gnu.org/licenses/>.
+ * ==================================================================
+ */
+package org.dc.bco.bcozy.view.devicepanes;
+
+import de.jensd.fx.glyphs.materialicons.MaterialIcon;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import org.dc.bco.dal.remote.unit.DALRemoteService;
+import org.dc.bco.dal.remote.unit.MotionSensorRemote;
+import org.dc.jul.exception.CouldNotPerformException;
+import org.dc.jul.exception.printer.ExceptionPrinter;
+import org.dc.jul.exception.printer.LogLevel;
+import org.dc.jul.pattern.Observable;
+import javafx.application.Platform;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import org.dc.bco.bcozy.view.Constants;
+import org.dc.bco.bcozy.view.SVGIcon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rst.homeautomation.state.MotionStateType.MotionState.State;
+import rst.homeautomation.unit.MotionSensorType.MotionSensor;
+
+/**
+ * Created by timo on 15.01.16.
+ */
+public class MotionSensorPane extends UnitPane {
+    private static final Logger LOGGER = LoggerFactory.getLogger(BatteryPane.class);
+
+    private final MotionSensorRemote motionSensorRemote;
+    private final SVGIcon brightnessIcon;
+    private final Text brightnessStatus;
+    private final GridPane iconPane;
+    private final BorderPane headContent;
+
+    /**
+     * Constructor for the BatteryPane.
+     * @param brightnessSensorRemote motionSensorRemote
+     */
+    public MotionSensorPane(final DALRemoteService brightnessSensorRemote) {
+        this.motionSensorRemote = (MotionSensorRemote) brightnessSensorRemote;
+
+        headContent = new BorderPane();
+        brightnessIcon = new SVGIcon(MaterialIcon.BLUR_ON, Constants.SMALL_ICON, false);
+        brightnessStatus = new Text();
+        iconPane = new GridPane();
+
+        initUnitLabel();
+        initTitle();
+        initContent();
+        createWidgetPane(headContent);
+
+        initEffect();
+
+        this.motionSensorRemote.addObserver(this);
+    }
+
+    private void initEffect() {
+        State motionState = State.UNKNOWN;
+
+        try {
+            motionState = motionSensorRemote.getMotion().getValue();
+        } catch (CouldNotPerformException e) {
+            ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+        }
+        setMotionStateIcon(motionState);
+    }
+
+    private void setMotionStateIcon(final State motionState) {
+        if (motionState.equals(State.MOVEMENT)) {
+            brightnessIcon.setColor(Color.WHITE, Color.BLACK, Constants.THIN_STROKE);
+        } else if (motionState.equals(State.UNKNOWN)) {
+            brightnessIcon.setColor(Color.BLACK);
+        } else {
+            brightnessIcon.setColor(Color.TRANSPARENT);
+        }
+    }
+
+    @Override
+    protected void initTitle() {
+        brightnessStatus.getStyleClass().add(Constants.ICONS_CSS_STRING);
+
+        brightnessIcon.setColor(Color.TRANSPARENT);
+
+        iconPane.add(brightnessIcon, 0, 0);
+        iconPane.add(brightnessStatus, 1, 0);
+
+        headContent.setLeft(iconPane);
+        headContent.setCenter(new Label(super.getUnitLabel()));
+        //Padding values are not available here
+        headContent.prefHeightProperty().set(iconPane.getHeight() + Constants.INSETS);
+    }
+
+    @Override
+    protected void initContent() {
+        //No body content.
+    }
+
+    @Override
+    protected void initUnitLabel() {
+        String unitLabel = Constants.UNKNOWN_ID;
+        try {
+            unitLabel = this.motionSensorRemote.getData().getLabel();
+        } catch (CouldNotPerformException e) {
+            ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+        }
+        setUnitLabel(unitLabel);
+    }
+
+    @Override
+    public DALRemoteService getDALRemoteService() {
+        return motionSensorRemote;
+    }
+
+    @Override
+    void removeObserver() {
+        this.motionSensorRemote.removeObserver(this);
+    }
+
+    @Override
+    public void update(final Observable observable, final Object motionSensor) throws java.lang.Exception {
+        Platform.runLater(() -> {
+            final State motionState = ((MotionSensor) motionSensor).getMotionState().getValue();
+            setMotionStateIcon(motionState);
+        });
+    }
+}
