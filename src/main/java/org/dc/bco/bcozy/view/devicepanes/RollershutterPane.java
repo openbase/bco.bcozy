8ff384e01517877ab864943333965c511cb97ea3
agatting <agatting@techfak.uni-bielefeld.de>
2016-01-24 22:30:54 +0100

    overworked RollershutterPane

--- src/main/java/org/dc/bco/bcozy/view/devicepanes/RollershutterPane.java
+++ src/main/java/org/dc/bco/bcozy/view/devicepanes/RollershutterPane.java
@@ -22,10 +22,11 @@ import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 import org.dc.bco.bcozy.view.SVGIcon;
 import org.dc.bco.dal.remote.unit.DALRemoteService;
 import org.dc.bco.dal.remote.unit.RollershutterRemote;
@@ -54,20 +55,22 @@ public class RollershutterPane extends UnitPane {
     private final HBox bodyContent;
     private final SVGIcon rollerShutterIconBackground;
     private final SVGIcon rollerShutterIconForeground;
-    private final Group rollerShutterIcon;
     private final Rectangle clip;
+    private final Text rollerShutterStatus;
+    private final GridPane iconPane;
 
     /**
-     * Constructor for a RollershutterPane.
+     * Constructor for a RollerShutterPane.
      * @param rollerShutterRemote DALRemoteService
      */
     public RollershutterPane(final DALRemoteService rollerShutterRemote) {
         this.rollershutterRemote = (RollershutterRemote) rollerShutterRemote;
 
         rollerShutterIconBackground = new SVGIcon(MaterialDesignIcon.FORMAT_ALIGN_JUSTIFY, Constants.SMALL_ICON, false);
-        rollerShutterIconForeground = new SVGIcon(MaterialDesignIcon.FORMAT_ALIGN_JUSTIFY, Constants.SMALL_ICON, false);
-        rollerShutterIcon = new Group();
+        rollerShutterIconForeground = new SVGIcon(MaterialDesignIcon.FORMAT_ALIGN_JUSTIFY, Constants.SMALL_ICON, true);
         clip = new Rectangle();
+        rollerShutterStatus = new Text();
+        iconPane = new GridPane();
 
         headContent = new BorderPane();
         bodyContent = new HBox();
@@ -76,25 +79,44 @@ public class RollershutterPane extends UnitPane {
         initTitle();
         initContent();
         createWidgetPane(headContent, bodyContent);
-
-        try {
-            initEffect();
-        } catch (CouldNotPerformException e) {
-            ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
-        }
+        initEffect();
 
         this.rollershutterRemote.addObserver(this);
     }
 
     private void setEffectOpeningRatio(final double percentage) {
         ((Rectangle) rollerShutterIconForeground.getClip()).setHeight(Constants.SMALL_ICON * percentage);
+        rollerShutterStatus.setText((int) (Constants.ONE_HUNDRED * percentage) + "%");
+    }
+
+    private void initEffect() {
+        double openingPercentage = 0.0;
+        try {
+            openingPercentage = rollershutterRemote.getOpeningRatio();
+        } catch (CouldNotPerformException e) {
+            ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+        }
+
+        setEffectOpeningRatio(openingPercentage);
     }
 
     @Override
     protected void initTitle() {
-        headContent.setLeft(rollerShutterIcon);
+        rollerShutterStatus.getStyleClass().add("icons");
+
+        iconPane.add(rollerShutterIconBackground, 0, 0);
+        iconPane.add(rollerShutterIconForeground, 0, 0);
+        iconPane.add(rollerShutterStatus, 1, 0);
+
+        headContent.setLeft(iconPane);
         headContent.setCenter(new Label(super.getUnitLabel()));
         headContent.prefHeightProperty().set(Constants.SMALL_ICON + Constants.INSETS);
+
+        clip.setWidth(Constants.SMALL_ICON);
+        clip.setHeight(Constants.SMALL_ICON);
+
+        rollerShutterIconBackground.setColor(Color.YELLOW);
+        rollerShutterIconForeground.setClip(clip);
     }
 
     @Override
@@ -118,84 +140,75 @@ public class RollershutterPane extends UnitPane {
         buttonOpen.setGraphic(arrowDoubleUP);
         buttonClose.setGraphic(arrowDoubleDown);
 
-        final EventHandler<MouseEvent> sendingTotalOpening = event -> {
-            new Thread(new Task() {
-                @Override
-                protected Object call() throws java.lang.Exception {
-                    try {
-                        rollershutterRemote.setOpeningRatio(0.0);
-                    } catch (CouldNotPerformException e) {
-                        ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
-                    }
-                    return null;
+        final EventHandler<MouseEvent> sendingTotalOpening = event -> new Thread(new Task() {
+            @Override
+            protected Object call() throws Exception {
+                try {
+                    rollershutterRemote.setOpeningRatio(0.0);
+                } catch (CouldNotPerformException e) {
+                    ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
                 }
-            }).start();
-        };
+                return null;
+            }
+        }).start();
 
-        final EventHandler<MouseEvent> sendingTotalClosing = event -> {
-            new Thread(new Task() {
-                @Override
-                protected Object call() throws java.lang.Exception {
-                    try {
-                        rollershutterRemote.setOpeningRatio(1.0);
-                    } catch (CouldNotPerformException e) {
-                        ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
-                    }
-                    return null;
+        final EventHandler<MouseEvent> sendingTotalClosing = event -> new Thread(new Task() {
+            @Override
+            protected Object call() throws Exception {
+                try {
+                    rollershutterRemote.setOpeningRatio(1.0);
+                } catch (CouldNotPerformException e) {
+                    ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
                 }
-            }).start();
-        };
+                return null;
+            }
+        }).start();
 
-        final EventHandler<MouseEvent> sendingUpLive = event -> {
-            new Thread(new Task() {
-                @Override
-                protected Object call() throws java.lang.Exception {
-                    try {
-                        if (rollershutterRemote.getOpeningRatio() >= Constants.ROLLERSHUTTER_STEP) {
-                            rollershutterRemote.setOpeningRatio(rollershutterRemote.getOpeningRatio()
-                                    - Constants.ROLLERSHUTTER_STEP);
-                        }
-                        //rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.UP);
-                    } catch (CouldNotPerformException e) {
-                        ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+        final EventHandler<MouseEvent> sendingUpLive = event -> new Thread(new Task() {
+            @Override
+            protected Object call() throws Exception {
+                try {
+                    if (rollershutterRemote.getOpeningRatio() >= Constants.ROLLERSHUTTER_STEP) {
+                        rollershutterRemote.setOpeningRatio(rollershutterRemote.getOpeningRatio()
+                                - Constants.ROLLERSHUTTER_STEP);
                     }
-                    return null;
-                }
-            }).start();
-        };
 
-        final EventHandler<MouseEvent> sendingStop = event -> {
-            new Thread(new Task() {
-                @Override
-                protected Object call() throws java.lang.Exception {
-                    try {
-                        rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.STOP);
-                    } catch (CouldNotPerformException e) {
-                        ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
-                    }
-                    return null;
-                }
-            }).start();
-        };
+                    rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.UP);
 
-        final EventHandler<MouseEvent> sendingDownLive = event -> {
-            new Thread(new Task() {
-                @Override
-                protected Object call() throws java.lang.Exception {
-                    try {
-                        if (rollershutterRemote.getOpeningRatio() <= Constants.ROLLERSHUTTER_MAX_VALUE
-                                - Constants.ROLLERSHUTTER_STEP) {
-                            rollershutterRemote.setOpeningRatio(rollershutterRemote.getOpeningRatio()
-                                    + Constants.ROLLERSHUTTER_STEP);
-                        }
-                        //rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.DOWN);
-                    } catch (CouldNotPerformException e) {
-                        ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
-                    }
-                    return null;
+                } catch (CouldNotPerformException e) {
+                    ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
                 }
-            }).start();
-        };
+                return null;
+            }
+        }).start();
+
+        final EventHandler<MouseEvent> sendingStop = event -> new Thread(new Task() {
+            @Override
+            protected Object call() throws Exception {
+                try {
+                    rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.STOP);
+                } catch (CouldNotPerformException e) {
+                    ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+                }
+                return null;
+            }
+        }).start();
+
+        final EventHandler<MouseEvent> sendingDownLive = event -> new Thread(new Task() {
+            @Override
+            protected Object call() throws Exception {
+                try {
+                    if (rollershutterRemote.getOpeningRatio() <= Constants.ROLLERSHUTTER_MAX_VALUE) {
+                        rollershutterRemote.setOpeningRatio(rollershutterRemote.getOpeningRatio()
+                                + Constants.ROLLERSHUTTER_STEP);
+                    }
+                    //rollershutterRemote.setShutter(ShutterStateType.ShutterState.State.DOWN);
+                } catch (CouldNotPerformException e) {
+                    ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
+                }
+                return null;
+            }
+        }).start();
 
         buttonOpen.setOnMouseClicked(sendingTotalOpening);
         buttonClose.setOnMouseClicked(sendingTotalClosing);
@@ -206,7 +219,8 @@ public class RollershutterPane extends UnitPane {
         //buttonDown.setOnMousePressed(sendingDownLive);
         //buttonDown.setOnMouseReleased(sendingStop);
 
-        bodyContent.getChildren().addAll(buttonUp, buttonDown, buttonOpen, buttonClose);
+        bodyContent.getChildren().addAll(buttonOpen, buttonUp, buttonDown, buttonClose);
+        bodyContent.setAlignment(Pos.CENTER);
         //CHECKSTYLE.OFF: MagicNumber
         bodyContent.prefHeightProperty().set(100 + Constants.INSETS);
         //CHECKSTYLE.ON: MagicNumber
@@ -221,19 +235,6 @@ public class RollershutterPane extends UnitPane {
             ExceptionPrinter.printHistory(e, LOGGER, LogLevel.ERROR);
         }
         setUnitLabel(unitLabel);
-    }
-
-    private void initEffect() throws CouldNotPerformException {
-        final double openingPercentage = rollershutterRemote.getOpeningRatio();
-
-        clip.setWidth(Constants.SMALL_ICON);
-        clip.setHeight(Constants.SMALL_ICON * openingPercentage);
-
-        rollerShutterIconBackground.setColor(Color.GRAY);
-        rollerShutterIconForeground.setColor(Color.BLACK);
-        rollerShutterIconForeground.setClip(clip);
-
-        rollerShutterIcon.getChildren().addAll(rollerShutterIconBackground, rollerShutterIconForeground);
     }
 
     @Override
