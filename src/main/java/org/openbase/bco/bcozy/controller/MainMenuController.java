8d78bc18916f4c9aaecb1b9d03af0e2d267d08c6
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2016-06-17 14:44:27 +0200

    replace 2 lambda expressions so that maybe travis can build it

--- src/main/java/org/openbase/bco/bcozy/controller/MainMenuController.java
+++ src/main/java/org/openbase/bco/bcozy/controller/MainMenuController.java
@@ -31,6 +31,8 @@ import org.openbase.bco.bcozy.view.mainmenupanes.SettingsPane;
 
 import java.util.Locale;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 /**
  * Created by hoestreich on 11/24/15.
@@ -63,8 +65,22 @@ public class MainMenuController {
         settingsPane.getStatusIcon().setOnMouseClicked(event -> showHideMainMenu(foregroundPane));
         availableUsersPane.getStatusIcon().setOnMouseClicked(event -> showHideMainMenu(foregroundPane));
         connectionPane.getStatusIcon().setOnMouseClicked(event -> showHideMainMenu(foregroundPane));
-        settingsPane.getThemeChoice().setOnAction(event -> chooseTheme());
-        settingsPane.getLanguageChoice().setOnAction(event -> chooseLanguage());
+//        settingsPane.getThemeChoice().setOnAction(event -> chooseTheme());
+        settingsPane.getThemeChoice().setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                chooseTheme();
+            }
+        });
+//        settingsPane.getLanguageChoice().setOnAction(event -> chooseLanguage());
+        settingsPane.getLanguageChoice().setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                chooseLanguage();
+            }
+        });
         //Necessary to ensure that the first change is not missed by the ChangeListener
         settingsPane.getThemeChoice().getSelectionModel().select(0);
         settingsPane.getLanguageChoice().getSelectionModel().select(0);
