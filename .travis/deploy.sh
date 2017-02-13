13832c3f8363460e669ecdda7025c2b50524e44c
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-02-13 17:06:10 +0100

    travis mvn build settings optimized by avoid double build and double force of depentendy updates.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,1 +1,5 @@
-mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B
+#!/bin/bash
+set -ev
+echo ### start deployment...
+mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q
+echo ### deployment successfully finished
