455d1edba64a8bfe31a669ec8449d54fcc8430f0
Marian Pohling <mpohling@cit-ec.uni-bielefeld.de>
2017-02-16 16:41:00 +0100

    forced travis to upgrade maven deps before deployment.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 echo ### start deployment...
-mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q
+mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B -q -U
 echo ### deployment successfully finished
