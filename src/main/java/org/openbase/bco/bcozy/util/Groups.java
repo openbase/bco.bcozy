e620ea73cac34d4197c2300da2d6e6c5377cba2b
Vanessa da Silva <vdasilva@techfak.uni-bielefeld.de>
2017-08-27 22:20:51 +0200

    Add methods to Groups for adding end removing

--- src/main/java/org/openbase/bco/bcozy/util/Groups.java
+++ src/main/java/org/openbase/bco/bcozy/util/Groups.java
@@ -6,31 +6,45 @@ import javafx.collections.ObservableList;
 import javafx.util.StringConverter;
 import org.openbase.bco.registry.remote.Registries;
 import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.printer.ExceptionPrinter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rst.domotic.unit.UnitConfigType;
+import rst.domotic.unit.UnitTemplateType;
+import rst.domotic.unit.authorizationgroup.AuthorizationGroupConfigType;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @author vdasilva
  */
 public final class Groups {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Groups.class);
+
     public static ObservableList<UnitConfigType.UnitConfig> getGroups() {
         ObservableList<UnitConfigType.UnitConfig> groups = FXCollections.observableArrayList();
 
         try {
             setGroups(Registries.getUserRegistry().getAuthorizationGroupConfigs(), groups);
-        } catch (CouldNotPerformException | InterruptedException ex) {
-            // not synchronized yet...
-            // ex.printStackTrace();
+        } catch (CouldNotPerformException ex) {
+            ExceptionPrinter.printHistory(ex, LOGGER);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
         }
 
         try {
             Registries.getUserRegistry().addDataObserver((observable, userRegistryData) ->
                     setGroups(userRegistryData.getAuthorizationGroupUnitConfigList(), groups)
             );
-        } catch (InterruptedException | CouldNotPerformException ex) {
-            ex.printStackTrace();
+        } catch (CouldNotPerformException ex) {
+            ExceptionPrinter.printHistory(ex, LOGGER);
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
         }
 
         return groups;
@@ -68,5 +82,33 @@ public final class Groups {
         };
     }
 
+    public static void removeGroup(UnitConfigType.UnitConfig group) throws InterruptedException {
+        try {
+            Registries.getUserRegistry().removeAuthorizationGroupConfig(group);
+        } catch (CouldNotPerformException ex) {
+            ExceptionPrinter.printHistory(ex, LOGGER);
+        }
+    }
 
+    public static boolean addGroup(String groupName) throws InterruptedException {
+        try {
+            tryAddGroup(groupName).get(5, TimeUnit.SECONDS);
+            return true;
+        } catch (CouldNotPerformException | TimeoutException | ExecutionException ex) {
+            ExceptionPrinter.printHistory(ex, LOGGER);
+        }
+        return false;
+    }
+
+    public static Future<UnitConfigType.UnitConfig> tryAddGroup(String groupName)
+            throws CouldNotPerformException, InterruptedException {
+
+        UnitConfigType.UnitConfig newGroup = UnitConfigType.UnitConfig.newBuilder()
+                .setLabel(groupName)
+                .setType(UnitTemplateType.UnitTemplate.UnitType.AUTHORIZATION_GROUP)
+                .setAuthorizationGroupConfig(AuthorizationGroupConfigType.AuthorizationGroupConfig.newBuilder())
+                .build();
+
+        return Registries.getUserRegistry().registerAuthorizationGroupConfig(newGroup);
+    }
 }
