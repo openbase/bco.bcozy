742de64044d21f5cbcbf9011023479b4322075f7
Vanessa da Silva <vdasilva@techfak.uni-bielefeld.de>
2017-08-27 22:58:39 +0200

    Add methods to get localized text

--- src/main/java/org/openbase/bco/bcozy/model/LanguageSelection.java
+++ src/main/java/org/openbase/bco/bcozy/model/LanguageSelection.java
@@ -1,30 +1,40 @@
 /**
  * ==================================================================
- *
+ * <p>
  * This file is part of org.openbase.bco.bcozy.
- *
+ * <p>
  * org.openbase.bco.bcozy is free software: you can redistribute it and modify
  * it under the terms of the GNU General Public License (Version 3)
  * as published by the Free Software Foundation.
- *
+ * <p>
  * org.openbase.bco.bcozy is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with org.openbase.bco.bcozy. If not, see <http://www.gnu.org/licenses/>.
  * ==================================================================
  */
 package org.openbase.bco.bcozy.model;
 
-import java.util.Locale;
-import java.util.Observable;
+import org.openbase.bco.bcozy.view.Constants;
+import org.openbase.jul.exception.printer.ExceptionPrinter;
+import org.openbase.jul.exception.printer.LogLevel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.*;
 
 /**
  * Created by hoestreich on 1/2/16.
+ *
+ * @author vdasilva
  */
 public final class LanguageSelection extends Observable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LanguageSelection.class);
+
 
     /**
      * Singleton instance.
@@ -34,10 +44,12 @@ public final class LanguageSelection extends Observable {
     /**
      * Private constructor to deny manual instantiation.
      */
-    private LanguageSelection() { }
+    private LanguageSelection() {
+    }
 
     /**
      * Singleton Pattern.
+     *
      * @return the singleton instance of the language selection observable
      */
     public static LanguageSelection getInstance() {
@@ -51,6 +63,7 @@ public final class LanguageSelection extends Observable {
 
     /**
      * Setter method to allow changing the language and notifying all gui elements to adapt afterwards.
+     *
      * @param selectedLocale the new locale which should be set as default.
      */
     public void setSelectedLocale(final Locale selectedLocale) {
@@ -58,4 +71,42 @@ public final class LanguageSelection extends Observable {
         Locale.setDefault(selectedLocale);
         notifyObservers(Locale.getDefault());
     }
+
+    /**
+     * Retunrns the localized text for the given identifier.
+     *
+     * @param identifier the identifier
+     * @return the localized string
+     */
+    public static String getLocalized(String identifier) {
+        Objects.requireNonNull(identifier);
+
+        String text;
+        try {
+            text = ResourceBundle.getBundle(Constants.LANGUAGE_RESOURCE_BUNDLE, Locale.getDefault()).getString(identifier);
+        } catch (MissingResourceException ex) {
+            ExceptionPrinter.printHistory("Could not resolve Identifier[" + identifier + "]", ex, LOGGER, LogLevel.WARN);
+            text = identifier;
+        }
+
+        return text;
+    }
+
+    /**
+     * Adds an Listener to the given identifier.
+     * The Listener is called, each time the language changed and on attach.
+     *
+     * @param identifier               the identifier
+     * @param onLanguageChangeListener the listener for this identifier
+     */
+    public static void addObserverFor(String identifier, OnLanguageChangeListener onLanguageChangeListener) {
+        getInstance().addObserver((o, arg) -> onLanguageChangeListener.onLanguageChange(Locale.getDefault(),
+                getLocalized(identifier)));
+        onLanguageChangeListener.onLanguageChange(Locale.getDefault(), getLocalized(identifier));
+    }
+
+    public interface OnLanguageChangeListener {
+        void onLanguageChange(Locale locale, String text);
+    }
+
 }
