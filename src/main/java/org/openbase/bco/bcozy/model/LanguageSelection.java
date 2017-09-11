eda605db4e66a27a2ecc959c0d497c8a8ac88976
Vanessa da Silva <vdasilva@techfak.uni-bielefeld.de>
2017-09-11 16:41:18 +0200

    removed <p>s

--- src/main/java/org/openbase/bco/bcozy/model/LanguageSelection.java
+++ src/main/java/org/openbase/bco/bcozy/model/LanguageSelection.java
@@ -1,17 +1,16 @@
 /**
  * ==================================================================
- * <p>
  * This file is part of org.openbase.bco.bcozy.
- * <p>
+ *
  * org.openbase.bco.bcozy is free software: you can redistribute it and modify
  * it under the terms of the GNU General Public License (Version 3)
  * as published by the Free Software Foundation.
- * <p>
+ *
  * org.openbase.bco.bcozy is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with org.openbase.bco.bcozy. If not, see <http://www.gnu.org/licenses/>.
  * ==================================================================
