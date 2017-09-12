0696b6d008f04930125ea280ada2a709fb46f791
Marian Pohling <divine@openbase.org>
2017-09-12 13:37:49 +0200

    Update ObserverText.java

--- src/main/java/org/openbase/bco/bcozy/view/ObserverText.java
+++ src/main/java/org/openbase/bco/bcozy/view/ObserverText.java
@@ -1,17 +1,17 @@
-/*
+/**
  * ==================================================================
- * 
+ *
  * This file is part of org.openbase.bco.bcozy.
- * 
+ *
  * org.openbase.bco.bcozy is free software: you can redistribute it and modify
  * it under the terms of the GNU General Public License (Version 3)
  * as published by the Free Software Foundation.
- * 
+ *
  * org.openbase.bco.bcozy is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with org.openbase.bco.bcozy. If not, see <http://www.gnu.org/licenses/>.
  * ==================================================================
