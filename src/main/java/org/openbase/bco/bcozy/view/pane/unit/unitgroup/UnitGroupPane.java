1e0a51235c643ad942c83a33f60d499ac61adcb7
Divine Threepwood <divine@openbase.org>
2017-11-27 16:30:30 +0100

    implement unit group pane

---
+++ src/main/java/org/openbase/bco/bcozy/view/pane/unit/unitgroup/UnitGroupPane.java
@@ -0,0 +1,75 @@
+/**
+ * ==================================================================
+ *
+ * This file is part of org.openbase.bco.bcozy.
+ *
+ * org.openbase.bco.bcozy is free software: you can redistribute it and modify
+ * it under the terms of the GNU General Public License (Version 3)
+ * as published by the Free Software Foundation.
+ *
+ * org.openbase.bco.bcozy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with org.openbase.bco.bcozy. If not, see <http://www.gnu.org/licenses/>.
+ * ==================================================================
+ */
+package org.openbase.bco.bcozy.view.pane.unit.unitgroup;
+
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
+import java.util.concurrent.Future;
+import javafx.scene.paint.Color;
+import org.openbase.bco.bcozy.view.pane.unit.AbstractUnitPane;
+import org.openbase.bco.dal.remote.unit.unitgroup.UnitGroupRemote;
+import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.printer.ExceptionPrinter;
+import org.openbase.jul.exception.printer.LogLevel;
+import rst.domotic.state.PowerStateType.PowerState;
+import rst.domotic.unit.unitgroup.UnitGroupDataType.UnitGroupData;
+
+/**
+ * @author <a href="mailto:divine@openbase.org">Divine Threepwood</a>
+ */
+public class UnitGroupPane extends AbstractUnitPane<UnitGroupRemote, UnitGroupData> {
+
+    /**
+     * Constructor for the PowerSwitchPane.
+     *
+     */
+    public UnitGroupPane() {
+        super(UnitGroupRemote.class, true);
+        getIcon().setForegroundIcon(MaterialDesignIcon.POWER);
+    }
+
+    @Override
+    public void updateDynamicContent() {
+        super.updateDynamicContent();
+
+        PowerState.State state = PowerState.State.UNKNOWN;
+
+        try {
+            state = getUnitRemote().getData().getPowerState().getValue();
+        } catch (CouldNotPerformException ex) {
+            ExceptionPrinter.printHistory(ex, LOGGER, LogLevel.DEBUG);
+        }
+
+        switch (state) {
+            case ON:
+                getIcon().setForegroundIconColor(Color.GREEN);
+                setPrimaryActivationWithoutNotification(Boolean.TRUE);
+                break;
+            case OFF:
+                getIcon().setForegroundIconColor(Color.BLACK);
+                setPrimaryActivationWithoutNotification(Boolean.FALSE);
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected Future applyPrimaryActivationUpdate(final boolean activation) throws CouldNotPerformException {
+        return (activation) ? getUnitRemote().setPowerState(PowerState.State.ON) : getUnitRemote().setPowerState(PowerState.State.OFF);
+    }
+}
