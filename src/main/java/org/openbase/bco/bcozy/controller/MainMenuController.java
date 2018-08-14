034bb0c37ccd7a958c8055295416b52ff8906cc3
Divine Threepwood <divine@openbase.org>
2018-08-14 23:49:18 +0200

    manual feature-check-expired-session merge

--- src/main/java/org/openbase/bco/bcozy/controller/MainMenuController.java
+++ src/main/java/org/openbase/bco/bcozy/controller/MainMenuController.java
@@ -18,11 +18,20 @@
  */
 package org.openbase.bco.bcozy.controller;
 
+import javafx.application.Platform;
+import org.openbase.bco.authentication.lib.SessionManager;
 import org.openbase.bco.bcozy.view.ForegroundPane;
 import org.openbase.bco.bcozy.view.mainmenupanes.AvailableUsersPane;
 import org.openbase.bco.bcozy.view.mainmenupanes.LoginPane;
+import org.openbase.bco.registry.remote.Registries;
+import org.openbase.jul.exception.CouldNotPerformException;
+import org.openbase.jul.exception.InvalidStateException;
+import org.openbase.jul.exception.printer.ExceptionPrinter;
+import org.openbase.jul.schedule.GlobalCachedExecutorService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import rst.domotic.unit.UnitConfigType.UnitConfig;
+import rst.domotic.unit.UnitTemplateType.UnitTemplate.UnitType;
 
 /**
  * Created by hoestreich on 11/24/15.
@@ -40,19 +49,110 @@ public class MainMenuController {
      * @param foregroundPane The foregroundPane allows to access all necessary gui elements
      */
     public MainMenuController(final ForegroundPane foregroundPane) {
-        init(foregroundPane);
-    }
-
-    public void init(final ForegroundPane foregroundPane) {
         loginPane = foregroundPane.getMainMenu().getLoginPane();
         availableUsersPane = foregroundPane.getMainMenu().getAvailableUsersPanePane();
+        loginPane.getLoginBtn().setOnAction(event -> loginUser());
+        loginPane.getLogoutBtn().setOnAction(event -> resetLogin());
+        loginPane.getPasswordField().setOnAction(event -> loginUser());
+        loginPane.getNameTxt().setOnAction(event -> loginUser());
+        loginPane.getNameTxt().setOnKeyTyped(event -> resetWrongInput());
+        loginPane.getPasswordField().setOnKeyTyped(event -> resetWrongInput());
         loginPane.getStatusIcon().setOnMouseClicked(event -> showHideMainMenu(foregroundPane));
         availableUsersPane.getStatusIcon().setOnMouseClicked(event -> showHideMainMenu(foregroundPane));
         foregroundPane.getMainMenu().getMainMenuFloatingButton().setOnAction(event -> showHideMainMenu(foregroundPane));
+
+        SessionManager.getInstance().addLoginObserver((observable, userAtClientId) -> onLoggedInChanged(userAtClientId));
+
+    }
+
+    private void onLoggedInChanged(final String userAtClientId) {
+        System.err.println("MainMenuController.onLoggedInChanged: " + userAtClientId);
+        if (userAtClientId.equals("@")) {
+            afterLogout();
+        } else {
+            afterLogin();
+        }
+    }
+
+    private void afterLogin() {
+
+    }
+
+    private void afterLogout() {
+        Platform.runLater(() -> {
+            if (loginPane.getInputWrongLbl().isVisible()) {
+                loginPane.resetUserOrPasswordWrong();
+            }
+            loginPane.getNameTxt().setText("");
+            loginPane.getPasswordField().setText("");
+            loginPane.getLoggedInUserLbl().setText("");
+            loginPane.setState(LoginPane.State.LOGINACTIVE);
+        });
+    }
+
+    private void startLogin() {
+        loginPane.setState(LoginPane.State.LOGINACTIVE);
+    }
+
+    private void resetWrongInput() {
+        if (loginPane.getInputWrongLbl().isVisible()) {
+            loginPane.resetUserOrPasswordWrong();
+        }
+    }
+
+    private void loginUser() {
+        GlobalCachedExecutorService.submit(() -> {
+            try {
+                loginUserAsync();
+            } catch (InterruptedException ex) {
+                ExceptionPrinter.printHistory("Could not login!", ex, LOGGER);
+            }
+        });
+    }
+
+    private void loginUserAsync() throws InterruptedException {
+        SessionManager sessionManager = SessionManager.getInstance();
+
+        try {
+
+            // sessionManager.login(Registries.getUserRegistry().getUserIdByUserName(loginPane.getNameTxt().getText()),
+            // ##### reimplemented because "getUserIdByUserName" not included in current master api.
+            final String username = loginPane.getNameTxt().getText();
+            String userUnitId = null;
+            for (final UnitConfig userUnitConfig : Registries.getUnitRegistry().getUnitConfigs(UnitType.USER)) {
+                if (userUnitConfig.getUserConfig().getUserName().equals(username)) {
+                    userUnitId = userUnitConfig.getId();
+                }
+            }
+
+            if (userUnitId == null) {
+                throw new InvalidStateException("username does not exists!");
+            }
+            // #####
+
+            sessionManager.login(userUnitId, loginPane.getPasswordField().getText());
+
+            Platform.runLater(() -> {
+                loginPane.resetUserOrPasswordWrong();
+                loginPane.getLoggedInUserLbl().setText(loginPane.getNameTxt().getText());
+                loginPane.getNameTxt().setText("");
+                loginPane.getPasswordField().setText("");
+                loginPane.setState(LoginPane.State.LOGOUT);
+            });
+        } catch (CouldNotPerformException ex) {
+            Platform.runLater(() -> {
+                loginPane.indicateUserOrPasswordWrong();
+            });
+        } catch (java.lang.OutOfMemoryError error) {
+            LOGGER.error(error.getMessage());
+        }
+    }
+
+    private void resetLogin() {
+        SessionManager.getInstance().logout();
     }
 
     private void showHideMainMenu(final ForegroundPane foregroundPane) {
-        //TODO: Resize the pain correctly
         if (foregroundPane.getMainMenu().isMaximized()) {
             foregroundPane.getMainMenu().minimizeMainMenu();
         } else {
