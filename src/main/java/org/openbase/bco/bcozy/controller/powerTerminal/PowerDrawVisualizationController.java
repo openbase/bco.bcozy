3ff2002003a9f40b1342e1704643476d2b064823
Svenja Kenneweg <skenneweg@techfak.uni-bielefeld.de>
2019-05-17 22:34:03 +0200

    Added WebView

--- src/main/java/org/openbase/bco/bcozy/controller/powerTerminal/PowerDrawVisualizationController.java
+++ src/main/java/org/openbase/bco/bcozy/controller/powerTerminal/PowerDrawVisualizationController.java
@@ -1,4 +1,23 @@
 package org.openbase.bco.bcozy.controller.powerTerminal;
 
-public class PowerDrawVisualizationController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PowerDrawVisualizationController implements Initializable {
+
+    @FXML
+    WebView webView;
+    private WebEngine webEngine;
+
+
+    @Override
+    public void initialize (URL url, ResourceBundle rb) {
+        webEngine = webView.getEngine();
+        webEngine.load("https://docs.influxdata.com/chronograf/v1.7/introduction/getting-started/");
+    }
 }
