4b2ad24e3b7a0c5a8aee2b7b9fb733e8782dd2dd
David Weinholz <david-weinholz@web.de>
2019-09-02 12:52:20 +0200

    PowerTerminalDbservice now uses locationremote

--- src/main/java/org/openbase/bco/bcozy/model/powerterminal/PowerTerminalDBService.java
+++ src/main/java/org/openbase/bco/bcozy/model/powerterminal/PowerTerminalDBService.java
@@ -1,24 +1,19 @@
 package org.openbase.bco.bcozy.model.powerterminal;
 
 import eu.hansolo.tilesfx.chart.ChartData;
-import org.influxdata.query.FluxRecord;
-import org.influxdata.query.FluxTable;
 import org.openbase.bco.bcozy.controller.powerterminal.chartattributes.DateRange;
 import org.openbase.bco.bcozy.controller.powerterminal.chartattributes.Interval;
-import org.openbase.bco.bcozy.model.InfluxDBHandler;
 import org.openbase.bco.bcozy.util.powerterminal.TimeLabelFormatter;
-import org.openbase.bco.dal.remote.layer.unit.PowerConsumptionSensorRemote;
-import org.openbase.bco.dal.remote.layer.unit.Units;
 import org.openbase.bco.dal.remote.layer.unit.location.LocationRemote;
-import org.openbase.jul.exception.CouldNotPerformException;
-import org.openbase.jul.exception.NotAvailableException;
 import org.openbase.jul.exception.printer.ExceptionPrinter;
+import org.openbase.type.domotic.database.RecordCollectionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 import static org.openbase.bco.bcozy.controller.powerterminal.PowerChartVisualizationController.INFLUXDB_FIELD_CONSUMPTION;
@@ -29,11 +24,14 @@ import static org.openbase.bco.bcozy.controller.powerterminal.PowerChartVisualiz
 public class PowerTerminalDBService {
     public static final long FIVE_MINUTES_IN_MILLISECONDS = 60000;
     private static final Logger LOGGER = LoggerFactory.getLogger(PowerTerminalDBService.class);
+    private static final LocationRemote locationRemote = new LocationRemote();
     public static final String UNIT_ID_GLOBAL_CONSUMPTION = "Global Consumption";
     public static final String UNIT_ID_LOCATION_CONSUMPTION = "Location Consumption";
 
+
     /**
      * Returns TilesFX Chartdata of the average power consumption during the given DateRange
+     *
      * @param dateRange DateRange in about which the database will be queried
      * @param unitId
      * @return Data about the average power consumption
@@ -67,14 +65,14 @@ public class PowerTerminalDBService {
         try {
             double value = 0;
             if (unitId.equals(UNIT_ID_GLOBAL_CONSUMPTION)) {
-                value = InfluxDBHandler.getAveragePowerConsumption(intervalSize.getInfluxIntervalString(),
-                        timeInSeconds - FIVE_MINUTES_IN_MILLISECONDS, timeInSeconds, INFLUXDB_FIELD_CONSUMPTION);
+                value = locationRemote.getAveragePowerConsumption(intervalSize.getInfluxIntervalString(),
+                        timeInSeconds - FIVE_MINUTES_IN_MILLISECONDS, timeInSeconds, INFLUXDB_FIELD_CONSUMPTION).get().getRecord(0).getValue();
             } else {
-                value = InfluxDBHandler.getAveragePowerConsumption(intervalSize.getInfluxIntervalString(), unitId,
-                        timeInSeconds - FIVE_MINUTES_IN_MILLISECONDS, timeInSeconds, INFLUXDB_FIELD_CONSUMPTION);
+                value = locationRemote.getAveragePowerConsumption(intervalSize.getInfluxIntervalString(), unitId,
+                        timeInSeconds - FIVE_MINUTES_IN_MILLISECONDS, timeInSeconds, INFLUXDB_FIELD_CONSUMPTION).get().getRecord(0).getValue();
             }
             data.add(new ChartData(TimeLabelFormatter.createTimeLabel(startAndEndTime, 0, intervalSize), value));
-        } catch (CouldNotPerformException e) {
+        } catch (InterruptedException | ExecutionException) {
             ExceptionPrinter.printHistory("Could not load datum!", e, LOGGER);
         }
         return data;
@@ -86,36 +84,37 @@ public class PowerTerminalDBService {
         long startTimeInSeconds = TimeUnit.MILLISECONDS.toSeconds(startTime.getTime());
         long endTimeInSeconds = TimeUnit.MILLISECONDS.toSeconds(endTime.getTime());
         try {
-            List<FluxTable> fluxTables;
+            RecordCollectionType.RecordCollection fluxTablesCollection;
             if (unitId.equals(UNIT_ID_GLOBAL_CONSUMPTION)) {
-                fluxTables = InfluxDBHandler.getAveragePowerConsumptionTables(
-                        interval, startTimeInSeconds, endTimeInSeconds, INFLUXDB_FIELD_CONSUMPTION);
+                fluxTablesCollection = locationRemote.getAveragePowerConsumptionTables(
+                        interval, startTimeInSeconds, endTimeInSeconds, INFLUXDB_FIELD_CONSUMPTION).get();
             } else {
-                fluxTables = InfluxDBHandler.getAveragePowerConsumptionTables(
-                        interval, unitId, startTimeInSeconds, endTimeInSeconds, INFLUXDB_FIELD_CONSUMPTION);
+                fluxTablesCollection = locationRemote.getAveragePowerConsumptionTables(
+                        interval, unitId, startTimeInSeconds, endTimeInSeconds, INFLUXDB_FIELD_CONSUMPTION).get();
+                ;
             }
-            data = convertToChartDataList(intervalSize, startTime, fluxTables);
-        } catch (CouldNotPerformException e) {
+            data = convertToChartDataList(intervalSize, startTime, fluxTablesCollection);
+        } catch (InterruptedException | ExecutionException e) {
             ExceptionPrinter.printHistory("Could not load chart data!", e, LOGGER);
         }
         return data;
     }
 
-    private static List<ChartData> convertToChartDataList(Interval intervalSize, Timestamp startTime, List<FluxTable> fluxTables) {
+    private static List<ChartData> convertToChartDataList(Interval intervalSize, Timestamp startTime, RecordCollectionType.RecordCollection fluxTablesCollection) {
         List<ChartData> data = new ArrayList<>();
-        if (fluxTables.size() == 0) {
+        if (fluxTablesCollection.getRecordList().size() == 0) {
             data.add(new ChartData("No Data!", 0));
         }
-        for (int i = 0; i < fluxTables.size(); i++) {
-            List<FluxRecord> records = fluxTables.get(i).getRecords();
-            for (FluxRecord fluxRecord : records) {
-                Double value = (Double) fluxRecord.getValueByKey("_value");
-                if (value == null) {
-                    value = 0.0;
-                }
-                data.add(i, new ChartData(TimeLabelFormatter.createTimeLabel(startTime, i, intervalSize), value));
+
+        for (int i = 0; i < fluxTablesCollection.getRecordList().size(); i++) {
+            Double value = fluxTablesCollection.getRecord(i).getValue();
+            if (value == null) {
+                value = 0.0;
             }
+
+            data.add(i, new ChartData(TimeLabelFormatter.createTimeLabel(startTime, i, intervalSize), value));
         }
+
         return data;
     }
 
