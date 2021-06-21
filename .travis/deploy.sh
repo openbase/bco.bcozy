1eee0398d140c6580fc855e8ab4cca37e8dbdc7a
Divine Threepwood <divine@openbase.org>
2021-06-21 21:58:58 +0200

    remove outdated travis files

--- .travis/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-echo '### start deployment...'
-if [ "${AUTO_DEPLOY}" = False ] ; then
-    echo '### Skip deloyment because auto deployment is disabled.'
-else 
-    mvn deploy -Pdeploy,sonatype --settings .travis/settings.xml -DskipTests=true -B
-    echo '### deployment successfully finished'
-fi
--- .travis/generate-doc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-echo '### start deployment...'
-#mvn javadoc:javadoc -B
-echo '### deployment successfully finished'
