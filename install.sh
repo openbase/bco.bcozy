5e4f55b218e48a4af230d02b68e21b45b9f44977
Divine Threepwood <divine@openbase.org>
2022-06-14 21:46:54 +0200

    fix install prefix

--- install.sh
+++ install.sh
@@ -7,13 +7,22 @@ ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 WHITE='\033[0;37m'
 
+export BCO_DIST="${BCO_DIST:=$HOME/usr/}"
+
+if [ ! -d ${BCO_DIST} ]; then
+    echo "No bco distribution found at: ${BCO_DIST}"
+    echo 'Please define the distribution installation target directory by setting the $BCO_DIST environment variable.'
+    exit 255
+fi
+
+
 APP_NAME='bcozy'
 APP_NAME=${BLUE}${APP_NAME}${NC}
 echo -e "=== ${APP_NAME} project ${WHITE}cleanup${NC}" &&
 ./mvnw clean --quiet $@ &&
 echo -e "=== ${APP_NAME} project ${WHITE}installation${NC}" &&
 ./mvnw install \
-        -DassembleDirectory=${prefix} \
+        -DassembleDirectory=${BCO_DIST} \
         -DskipTests=true \
         -Dmaven.test.skip=true \
         -Dlicense.skipAddThirdParty=true \
@@ -22,4 +31,4 @@ echo -e "=== ${APP_NAME} project ${WHITE}installation${NC}" &&
         -Dlicense.skipCheckLicense=true \
         -Dmaven.license.skip=true \
         --quiet $@ &&
-echo -e "=== ${APP_NAME} was ${GREEN}successfully${NC} installed to ${WHITE}${prefix}${NC}"
+echo -e "=== ${APP_NAME} was ${GREEN}successfully${NC} installed to ${WHITE}${BCO_DIST}${NC}"
